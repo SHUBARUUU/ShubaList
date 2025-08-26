360dd0e5a2720f20cb96d9b52dad6869a2f10ac4
SHUBARUUU <joshuaemmanuelbalois@gmail.com>
2025-08-27 01:39:47 +0800

    Added the add and remove in ShubaList Class

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
+        ShubaList<String> shuba = new ShubaList<>();
+
         System.out.println("My Very First Data Structure!");
 
+        shuba.addShuba("Joshua");
+        shuba.viewShuba(0);
+
     }//end of clas
+
 }//end of class
--- src/ShubaList.java
+++ src/ShubaList.java
@@ -1,2 +1,34 @@
-public class ShubaList {
-}
+// An object that builds the data structure from scratch
+// Objective for tonight:
+// Add method.
+// Remove method.
+
+public class ShubaList<Item> {
+    private Item[] datas;
+    private int size;
+
+    //Constructor - start up
+    public ShubaList(){
+        datas = (Item[]) new Object[10];
+        size = 0;
+    }
+//  Adding Shuba!
+    public Item[] addShuba(Item item){
+        datas[size++] = item;
+        return datas;
+    }
+//  Viewing Shuba!
+    public void viewShuba(int index){
+        System.out.println(datas[index]);
+    }
+
+
+}//end of class
+
+//LEARNINGS:
+// Casting = pretending something is a type (only works if compatible)
+// Generic Types <> = Exists only in compile time, not in runtime.
+// Using new Object[10] + (Item[]) in the constructor sets up your array properly
+// Non-Static: Object level, unique per object ; Static: Class level, shares the same values.
+
+
