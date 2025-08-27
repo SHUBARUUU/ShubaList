43ceca0663acf0bc1f1ca2e3f6fc2910cd6456f6
SHUBARUUU <joshuaemmanuelbalois@gmail.com>
2025-08-27 23:57:08 +0800

    Added the remove method and the getter of size... Doing commit for checkpoints

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,28 @@
 public class Main {
     public static void main(String[] args) {
+        // Testing and Debugging!
 
         ShubaList<String> shuba = new ShubaList<>();
 
         System.out.println("My Very First Data Structure!");
 
         shuba.addShuba("Joshua");
-        shuba.viewShuba(0);
+        shuba.addShuba("Lebron");
+        shuba.addShuba("James");
+        shuba.addShuba("Shubaruu");
+
+        for (int i = 0; i < shuba.sizeShuba(); i++) {
+            System.out.println(shuba.getShuba(i));
+        }
+        shuba.removeShuba(3);
+
+//        System.out.println(detector);
+        System.out.println();//
+
+        for (int i = 0; i < shuba.sizeShuba(); i++) {
+            System.out.println(shuba.getShuba(i));
+        }
+
 
     }//end of clas
 
--- src/ShubaList.java
+++ src/ShubaList.java
@@ -1,27 +1,51 @@
 // An object that builds the data structure from scratch
-// Objective for tonight:
-// Add method.
 // Remove method.
-
 public class ShubaList<Item> {
     private Item[] datas;
     private int size;
 
     //Constructor - start up
-    public ShubaList(){
+    @SuppressWarnings("unchecked")//Suppresses the warning.
+    public ShubaList() {
         datas = (Item[]) new Object[10];
         size = 0;
     }
-//  Adding Shuba!
-    public Item[] addShuba(Item item){
+
+//  Interactive Methods
+
+    //Adding Value Shuba!
+    public boolean addShuba(Item item) {
         datas[size++] = item;
-        return datas;
-    }
-//  Viewing Shuba!
-    public void viewShuba(int index){
-        System.out.println(datas[index]);
+        return true;
     }
 
+    //Getting Value Shuba!
+    public Item getShuba(int getIndex) {
+        return datas[getIndex];
+
+    }
+
+    //Removing Value Shuba!
+    public boolean removeShuba(int removeIndex) {
+        if (removeIndex < 0 || removeIndex > size)
+            return false;
+
+        for (int i = removeIndex; i < sizeShuba() - 1; i++) {
+            datas[i] = datas[i + 1];
+        }
+        datas[sizeShuba() - 1] = null;
+        size--;
+
+        return true;
+    }
+
+    //Array Size Shuba!
+    public int sizeShuba() {
+        return size;
+    }
+
+//  Object methods.
+    //Add the array resizing methods when full.
 
 }//end of class
 
@@ -30,5 +54,6 @@ public class ShubaList<Item> {
 // Generic Types <> = Exists only in compile time, not in runtime.
 // Using new Object[10] + (Item[]) in the constructor sets up your array properly
 // Non-Static: Object level, unique per object ; Static: Class level, shares the same values.
+// null is no value "marker" for every object -- default empty value
 
 
